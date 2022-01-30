8a7e3ee442b5b2ce342f5148ed1734ae9868f34a
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-01-30 13:55:56 -0500

    installed tailwind

--- Covigo/urls.py
+++ Covigo/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from Covigo import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
