9204daf92ad01f61bff2a79fc0d4f34a7a9cac0d
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-01-30 13:55:56 -0500

    installed tailwind

--- Covigo/settings.py
+++ Covigo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tailwind',
     'theme',
+    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'Covigo.urls'
