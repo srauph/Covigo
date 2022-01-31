95bdebbf11d6951004ecdea630a04031a04377cf
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-01-30 21:57:28 -0500

    deleted old views.py file

--- Covigo/urls.py
+++ Covigo/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from Covigo import views
 
 urlpatterns = [
     path('', include('dashboard.urls')),
