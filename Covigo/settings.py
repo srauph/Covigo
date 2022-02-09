7ee727e5e50f7266ae4a681ed0dbfd8aeb165fa8
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-02-08 22:40:09 -0500

    implemented login

--- Covigo/settings.py
+++ Covigo/settings.py
@@ -139,3 +139,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/dashboard'
+LOGOUT_REDIRECT_URL = '/accounts/login'
