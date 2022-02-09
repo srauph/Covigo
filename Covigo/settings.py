dc42995515291e1cc59e633d2fea203420477d76
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-08 22:17:20 -0500

    changed to mysql

--- Covigo/settings.py
+++ Covigo/settings.py
@@ -87,10 +87,16 @@ WSGI_APPLICATION = 'Covigo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Covigo',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
 
 
 # Password validation
