3b3c2dc100fea6edf1ebb5e746633f020682ce5a
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-11 01:34:50 -0500

    temp page to try out querying stuff

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from accounts.models import Staff
+
+
+def userlist(request):
+    staff = justGetAStaffForNowIdcAbtAnythingElse(1)
+    return render(request, 'accounts/list.html', {'staff': staff})
+
+
+def justGetAStaffForNowIdcAbtAnythingElse( user_id):
+    staff = Staff.objects.get(pk=user_id)
+    return staff
