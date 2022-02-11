9d001597d1aa3c1ff3eb3824403c89a93c224654
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-11 01:01:15 -0500

    made Staff and Patient models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Staff(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    phone_number = models.CharField(max_length=255)
+    address = models.TextField()
+    postal_code = models.CharField(max_length=255)
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+    staff = models.ForeignKey(
+        Staff,
+        on_delete=models.CASCADE,
+    )
+    phone_number = models.CharField(max_length=255)
+    address = models.TextField()
+    postal_code = models.CharField(max_length=255, blank=True, null=True)
+    is_confirmed = models.BooleanField(default=False)
+    is_recovered = models.BooleanField(default=False)
+    is_flagged = models.BooleanField(default=False)
+    is_quarantining = models.BooleanField(default=False)
+    code = models.CharField(max_length=255)
