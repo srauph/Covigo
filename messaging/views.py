5a833ffa7b07610c5515632bb35b74b35080ad74
Auvigoo <auvigooa@gmail.com>
2022-02-10 23:40:50 -0500

    Add routing for view conversation page

--- messaging/views.py
+++ messaging/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'messaging/index.html')
+
+def viewMessage(request):
+    return render(request, 'messaging/view-message.html')
