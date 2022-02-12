5d78e39bfe9cb5e29807ffbd1ed774add8a3239d
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-02-11 19:21:35 -0500

    fiished implemention patient/staff/profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from Covigo.exceptions import UserNotPatientNorStaffException, UserHasNoProfileException
 
 
-class Staff(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
@@ -10,6 +14,13 @@ class Staff(models.Model):
     phone_number = models.CharField(max_length=255)
     address = models.TextField()
     postal_code = models.CharField(max_length=255)
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
 
 
 class Patient(models.Model):
@@ -21,11 +32,23 @@ class Patient(models.Model):
         Staff,
         on_delete=models.CASCADE,
     )
-    phone_number = models.CharField(max_length=255)
-    address = models.TextField()
-    postal_code = models.CharField(max_length=255, blank=True, null=True)
     is_confirmed = models.BooleanField(default=False)
     is_recovered = models.BooleanField(default=False)
     is_flagged = models.BooleanField(default=False)
     is_quarantining = models.BooleanField(default=False)
     code = models.CharField(max_length=255)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    try:
+        instance.profile.save()
+    # TODO change Exception with the proper exception name later
+    except Exception:
+        print("User has no profile")
+    if hasattr(instance, 'patient'):
+        instance.patient.save()
+    elif hasattr(instance, 'staff'):
+        instance.staff.save()
+    else:
+        raise UserNotPatientNorStaffException
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-
-from accounts.models import Staff
 
 
 def userlist(request):
-    staff = justGetAStaffForNowIdcAbtAnythingElse(1)
-    return render(request, 'accounts/list.html', {'staff': staff})
+    return render(request, 'accounts/list.html', {
+        'users': User.objects.all()
+    })
 
 
-def justGetAStaffForNowIdcAbtAnythingElse( user_id):
-    staff = Staff.objects.get(pk=user_id)
-    return staff
+def justGetAStaffForNowIdcAbtAnythingElse(user_id):
+    user = User.objects.get(pk=user_id)
+    return user
