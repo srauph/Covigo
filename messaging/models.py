44e724154fdecc3c67af24ff44fc04a0fa786dc3
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-02-12 12:39:04 -0500

    implemented messaging model migrations

--- messaging/models.py
+++ messaging/models.py
@@ -2,20 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class MessageContent(models.Model):
-    author = models.ManyToManyField(
-        User,
-        related_name="authors",
-        through='MessageTitle',
-        through_fields=('')
+class MessageGroup(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='authored_messages'
     )
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='received_messages'
+    )
+    title = models.TextField(blank=True, null=True)
+    priority = models.IntegerField(blank=True, null=True)
+    seen = models.BooleanField(blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    date_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
 
 
-class MessageTitle(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message_content = models.ForeignKey(MessageContent, on_delete=models.CASCADE)
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['user', 'message_content'], name='')
-        ]
+class MessageContent(models.Model):
+    message = models.ForeignKey(
+        MessageGroup,
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        related_name='+'
+    )
+    content = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    date_updated = models.DateTimeField(blank=True, null=True)
 
