f63cbecd0d3b4e04048c1f304dbe3568867a1dee
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-11 23:48:49 -0500

    wrote symptom and patient_symptom models

--- symptoms/models.py
+++ symptoms/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import Patient
 
-# Create your models here.
+
+class Symptom:
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    is_active = models.BooleanField()
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+
+
+class PatientSymptom:
+    patient = models.OneToOneField(
+        Patient,
+        on_delete=models.CASCADE(),
+    )
+    symptom = models.OneToOneField(
+        Symptom,
+        on_delete=models.CASCADE()
+    )
+    data = models.TextField()
+    is_hidden = models.BooleanField()
+    is_viewed = models.BooleanField()
+    due_date = models.DateTimeField()
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
