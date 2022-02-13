239356b2a5e96fee378f6db79c0fd7125b1063ba
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-13 13:01:31 -0500

    hid everything behind login and did minor name refactoring

--- symptoms/views.py
+++ symptoms/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 
 
+@login_required
 @never_cache
 def list_symptoms(request):
     return render(request, 'symptoms/list.html')
 
 
+@login_required
 @never_cache
 def create(request):
     return render(request, 'symptoms/create.html')
 
 
+@login_required
 @never_cache
 def userid(request):
     return render(request, 'symptoms/userid.html')
