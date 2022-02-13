f61825f3764d144d50301aa8c25269e3f398a327
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-02-12 22:10:33 -0500

    finished models and made migrations

--- messaging/models.py
+++ messaging/models.py
@@ -4,18 +4,20 @@ from django.contrib.auth.models import User
 
 class MessageGroup(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='authored_messages'
+        User,
+        related_name='authored_messages',
+        on_delete=models.CASCADE
     )
     recipient = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='received_messages'
+        User,
+        related_name='received_messages',
+        on_delete=models.CASCADE
     )
     title = models.TextField(blank=True, null=True)
     priority = models.IntegerField(blank=True, null=True)
-    seen = models.BooleanField(blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True)
-    date_updated = models.DateTimeField(blank=True, null=True)
+    seen = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -27,11 +29,12 @@ class MessageContent(models.Model):
         on_delete=models.CASCADE
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name='+'
+        User,
+        related_name='+',
+        on_delete=models.CASCADE
     )
     content = models.TextField(blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True)
-    date_updated = models.DateTimeField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     # TODO make str method
