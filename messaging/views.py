dd996aa7add1045c1424edce388fbc8835d99916
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-13 01:38:50 -0500

    added never_cache decorators

--- messaging/views.py
+++ messaging/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 
 
+@never_cache
 def index(request):
     return render(request, 'messaging/index.html')
 
+
+@never_cache
 def composeMessage(request):
     return render(request, 'messaging/composeMessage.html')
