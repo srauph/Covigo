9ca582584f2aedcd6f0c48b109968e72dd3a7867
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-13 13:27:05 -0500

    renamed some stuff

--- messaging/urls.py
+++ messaging/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'messaging'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('view/userId/', views.view_message, name='view_message'),
+    path('view/user_id/', views.view_message, name='view_message'),
     path('compose_message/', views.compose_message, name='compose_message'),
 
 ]
