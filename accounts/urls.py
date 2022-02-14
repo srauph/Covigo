2373e54450f58294ca38436cd9462225530fc2c7
Amir Sadeghi <asadeqi@icloud.com>
2022-02-13 19:33:35 -0500

    added UI for group/add

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.list_users, name='list_users'),
+    path('access_control/group/add', views.add_group, name='add_group'),
 ]
