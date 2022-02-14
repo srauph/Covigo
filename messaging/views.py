ba51c89bc70d7e70eb36be8efbc92982ca410b19
Auvigoo <auvigooa@gmail.com>
2022-02-14 17:07:52 -0500

    Start view message function. Add message authorization checks, and able to send queried messages to template

--- messaging/views.py
+++ messaging/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.contrib.auth.models import User
+from django.db.models import Q
+from messaging.models import MessageGroup, MessageContent
 
 
 @login_required
@@ -17,12 +20,30 @@ def list_messages(request):
 
 @login_required
 @never_cache
-def view_message(request):
-    return render(request, 'messaging/view_message.html')
+def view_message(request, message_group_id=3):
+
+    # Check if user is authorized to view the messages with a specific message_group_id
+    current_user_id = request.user.id
+    # Where conditions for the queries
+    where1 = Q(id=message_group_id)
+    where2 = Q(author_id=current_user_id) | Q(recipient_id=current_user_id)
+    message_groups = MessageGroup.objects.filter(where1 & where2)
+
+    if message_groups:
+
+        messages = MessageContent.objects.filter(message_id=message_group_id)
+
+        return render(request, 'messaging/view_message.html', {
+            'context': messages
+        })
+    else:
+        return redirect('messaging:list_messages')
+
+
+
 
 
 @login_required
 @never_cache
 def compose_message(request):
     return render(request, 'messaging/compose_message.html')
-
