090c96af5f8aff83087dada6a3d642e5b255e611
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-02-13 23:06:22 -0500

    symptom creation backend

--- symptoms/views.py
+++ symptoms/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.cache import never_cache
+
+from symptoms.models import Symptom
 
 
 @never_cache
@@ -9,7 +11,43 @@ def list_symptoms(request):
 
 @never_cache
 def create(request):
-    return render(request, 'symptoms/create.html')
+    if request.method == 'POST':
+        symptom = Symptom()
+        symptom.name = request.POST.get('symptom_name')
+        symptom.description = request.POST.get('symptom_description')
+        symptom.save()
+
+        if request.POST.get('submit_and_return'):
+            return redirect('symptoms:list')
+
+        else:
+            return render(request, 'symptoms/create.html', {
+                'previous_symptom': symptom
+            })
+
+    else:
+        return render(request, 'symptoms/create.html')
+
+    # if request.method == 'POST' and 'submit_and_return' in request.POST:
+    #     new_symptom = Symptom(name=request.session['symptom_name'],
+    #                           description=request.session['symptom_description'],
+    #                           is_active=False,
+    #                           date_created=True,
+    #                           date_updated=True)
+    #     new_symptom.save()
+    #
+    # elif request.method == 'POST' and 'submit_and_duplicate' in request.POST:
+    #     new_symptom = Symptom(name=request.session['symptom_name'],
+    #                           description=request.session['symptom_description'],
+    #                           is_active=False,
+    #                           date_created=True,
+    #                           date_updated=True)
+    #     new_symptom.save()
+    #     new_symptom.pk = None
+    #     new_symptom.state.adding = True
+    #     new_symptom.save()
+
+    # return render(request, 'symptoms/create.html')
 
 
 @never_cache
