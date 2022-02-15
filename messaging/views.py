62b879ffa5da4b95a89e27c3ec3bec8a643b67a9
Auvigoo <auvigooa@gmail.com>
2022-02-14 23:56:25 -0500

    Fix URL with message group id, simplify view message function to return fewer variables

--- messaging/views.py
+++ messaging/views.py
@@ -20,31 +20,19 @@ def list_messages(request):
 
 @login_required
 @never_cache
-def view_message(request, message_group_id=3):
-    current_user_id = request.user.id
-    # Where conditions for the queries to check if user is authorized to view the messages with a specific
-    # message_group_id
-    where1 = Q(id=message_group_id)
-    where2 = Q(author_id=current_user_id) | Q(recipient_id=current_user_id)
-    message_group = MessageGroup.objects.filter(where1 & where2).first()
+def view_message(request, message_group_id):
+    current_user = request.user
+    # Filters for the queries to check if user is authorized to view the messages with a specific message_group_id
+    filter1 = Q(id=message_group_id)
+    filter2 = Q(author_id=current_user.id) | Q(recipient_id=current_user.id)
+    message_group = MessageGroup.objects.filter(filter1 & filter2).get()
 
     if message_group:
-
         messages = MessageContent.objects.filter(message_id=message_group_id)
-
-        # Identify the sender (current user) and receiver
-        # This is needed to get the Users names and decide the color of message boxes
-        sender = request.user
-        if message_group.author_id == current_user_id:
-            receiver = User.objects.get(id=message_group.recipient_id)
-        else:
-            receiver = User.objects.get(id=message_group.author_id)
 
         return render(request, 'messaging/view_message.html', {
             'message_group': message_group,
-            'messages': messages,
-            'sender': sender,
-            'receiver': receiver
+            'messages': messages
         })
     # User is not authorized to view this message group
     else:
