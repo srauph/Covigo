f2ca2627dafd406a56e0fb3d1a1b1082782c56da
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-02-14 21:08:25 -0500

    fixed the symptom_count_by_id function

--- symptoms/utils.py
+++ symptoms/utils.py
@@ -1,6 +1,16 @@
+from django.db.models import Count
 from symptoms.models import PatientSymptom
 
 
 # Input as [1] for symptom_id 1 or [1, 2] for multiple [#, #, ...]
 def symptom_count_by_id(symptom_id_list):
-    return PatientSymptom.objects.filter(symptom_id__in=symptom_id_list).count()
+    if not isinstance(symptom_id_list, list):
+        symptom_id_list = [symptom_id_list]
+
+    results = PatientSymptom.objects \
+        .filter(symptom_id__in=symptom_id_list) \
+        .values('symptom_id') \
+        .annotate(Count('symptom_id')) \
+        .order_by('symptom_id__count')
+
+    return results[0].get('symptom_id__count')
