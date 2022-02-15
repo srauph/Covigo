0122a04330b6ccc158dc6cc18b1646477078e60f
Auvigoo <auvigooa@gmail.com>
2022-02-14 19:00:05 -0500

    Make view messages template display messages dynamically.

--- messaging/views.py
+++ messaging/views.py
@@ -21,26 +21,34 @@ def list_messages(request):
 @login_required
 @never_cache
 def view_message(request, message_group_id=3):
-
     # Check if user is authorized to view the messages with a specific message_group_id
     current_user_id = request.user.id
     # Where conditions for the queries
     where1 = Q(id=message_group_id)
     where2 = Q(author_id=current_user_id) | Q(recipient_id=current_user_id)
-    message_groups = MessageGroup.objects.filter(where1 & where2)
+    message_group = MessageGroup.objects.filter(where1 & where2).first()
 
-    if message_groups:
+    if message_group:
 
         messages = MessageContent.objects.filter(message_id=message_group_id)
 
+        # Identify the sender (current user) and receiver
+        # This is needed to get the Users names and decide the color of message boxes
+        sender = User.objects.get(id=current_user_id)
+        if message_group.author_id == current_user_id:
+            receiver = User.objects.get(id=message_group.recipient_id)
+        else:
+            receiver = User.objects.get(id=message_group.author_id)
+
         return render(request, 'messaging/view_message.html', {
-            'context': messages
+            'message_group': message_group,
+            'messages': messages,
+            'sender': sender,
+            'receiver': receiver
         })
+    # User is not authorized to view this message group
     else:
         return redirect('messaging:list_messages')
-
-
-
 
 
 @login_required
