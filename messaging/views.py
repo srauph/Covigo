a01702d2a6f36102d052c390e7204e57cb21a727
Auvigoo <auvigooa@gmail.com>
2022-02-17 20:27:23 -0500

    Able to send replies

--- messaging/views.py
+++ messaging/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
-from django.contrib.auth.models import User
 from django.db.models import Q
 from messaging.models import MessageGroup, MessageContent
+from messaging.forms import ReplyForm
 
 
 @login_required
@@ -30,9 +30,27 @@ def view_message(request, message_group_id):
     if message_group:
         messages = MessageContent.objects.filter(message_id=message_group_id)
 
+        if request.method == 'POST':
+            reply_form = ReplyForm(request.POST)
+
+            if reply_form.is_valid():
+                # Add attributes before saving to db since they're not fields in the form class
+                new_reply = reply_form.save(commit=False)
+                new_reply.message = message_group
+                new_reply.author = current_user
+                # Save to db
+                new_reply.save()
+
+                # Reset the form
+                reply_form = ReplyForm()
+        # Initialize the reply form
+        else:
+            reply_form = ReplyForm()
+
         return render(request, 'messaging/view_message.html', {
             'message_group': message_group,
-            'messages': messages
+            'messages': messages,
+            'form': reply_form
         })
     # User is not authorized to view this message group
     else:
