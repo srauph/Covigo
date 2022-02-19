f2ee0127caee2c60a1b211512d290fe465ebd8ae
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-02-19 16:11:48 -0500

    just committing to pull from master

--- symptoms/forms.py
+++ symptoms/forms.py
@@ -10,7 +10,6 @@ class CreateSymptomForm(forms.ModelForm):
         label='Symptom Name',
         widget=forms.TextInput(attrs={
             'placeholder': 'e.g. fever, cough, cold, etc.',
-            'required': True,
             'size': 50,
             'class': 'w-full rounded border px-4 py-2'
         })
@@ -19,8 +18,6 @@ class CreateSymptomForm(forms.ModelForm):
         label='Symptom Description',
         widget=forms.Textarea(attrs={
             'placeholder': 'e.g. individual may lose sense of smell and/or taste, etc.',
-            'max_length': 100,
-            'required': True,
             'rows': 3,
             'cols': 50,
             'class': 'w-full px-4 py-2 rounded border align-middle'
