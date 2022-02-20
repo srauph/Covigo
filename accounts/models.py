f727fd98d3ed30457682038162afe0d966f5e8ad
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-02-20 00:29:01 -0500

    exception handling in save_user_profile

--- accounts/models.py
+++ accounts/models.py
@@ -71,4 +71,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, created, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except Profile.DoesNotExist:
+        Profile.objects.create(user=instance)
