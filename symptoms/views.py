66dd05954450ca8769c108489371e7956e108b19
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-02-27 20:13:16 -0500

    added the form validation messages for each symptom page (success and error alerts), will try to refactor testing for these accordingly now :)

--- symptoms/views.py
+++ symptoms/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from symptoms.models import Symptom, PatientSymptom
 from symptoms.forms import CreateSymptomForm
+from django.contrib import messages
 
 
 @login_required
@@ -36,15 +37,17 @@ def create_symptom(request):
                 create_symptom_form.save()
 
                 if request.POST.get('Create and Return'):
+                    messages.success(request, 'The symptom was created successfully.')
                     return redirect('symptoms:list_symptoms')
 
                 else:
+                    messages.success(request, 'The symptom was created successfully.')
                     return render(request, 'symptoms/create_symptom.html', {
                         'form': create_symptom_form
                     })
 
             else:
-                create_symptom_form.add_error('name', "This symptom name already exists for a given symptom.")
+                messages.error(request, 'The symptom was not created successfully: This symptom name already exists for a given symptom. Please change the symptom name.')
 
     else:
         create_symptom_form = CreateSymptomForm()
@@ -65,19 +68,18 @@ def edit_symptom(request, symptom_id):
     if request.method == 'POST':
         edit_symptom_form = CreateSymptomForm(request.POST, instance=symptom)
 
-        if symptom.name == edit_symptom_form.data.get('name') and symptom.description == edit_symptom_form.data.get('description'):
-            edit_symptom_form.add_error(None, "No edits made on this symptom. If you wish to make no changes, please click the \"Cancel\" button to go back to the list of symptoms.")
+        if not edit_symptom_form.has_changed():
+            messages.error(request, "The symptom was not edited successfully: No edits made on this symptom. If you wish to make no changes, please click the \"Cancel\" button to go back to the list of symptoms.")
+            return render(request, 'symptoms/edit_symptom.html', {
+                'form': edit_symptom_form
+            })
 
         if edit_symptom_form.is_valid():
             edit_symptom_form.save()
 
             if request.POST.get('Edit and Return'):
+                messages.success(request, 'The symptom was created successfully.')
                 return redirect('symptoms:list_symptoms')
-
-            else:
-                return render(request, 'symptoms/edit_symptom.html', {
-                    'form': edit_symptom_form
-                })
 
     else:
         edit_symptom_form = CreateSymptomForm(instance=symptom)
