723f959863560266ebff0fb16f8cf9ae10fc0eef
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2022-03-03 18:14:38 -0500

    Doctor patient report page now display reports
    * The reports are ordered by date, still have to also filter by flagging

--- status/views.py
+++ status/views.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.models import User
+from django.db.models import Max, Count
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from django.views.decorators.cache import never_cache
+
+from accounts.models import Patient, Staff
+from symptoms.models import PatientSymptom
 
 
 @login_required
@@ -12,6 +18,20 @@ def index(request):
 @login_required
 @never_cache
 def patient_reports(request):
+    doctor = request.user
+
+    # Get doctors patient name(s) and user id(s)
+    patient_ids = []
+    for users in Patient.objects.all():
+        if users.staff_id == doctor.id:
+            patient_ids.append(users.user_id)
+
+    # Return a QuerySet with all distinct reports from the doctors patients based on their updated date
+    # TODO filter it based on flagging first priority
+    dates = PatientSymptom.objects.select_related('user') \
+        .values('date_updated', 'user_id', 'user__first_name', 'user__last_name') \
+        .filter(user_id__in=patient_ids).order_by('date_updated').distinct().reverse()
+
     return render(request, 'status/patient-reports.html', {
-        'patient_reports': []
+        'patient_reports': dates
     })
