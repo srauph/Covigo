949d6bafeafbd59a091822f89c43c9185f60c705
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2022-03-04 20:12:46 -0500

    Testing modal still

--- status/urls.py
+++ status/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'status'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('patient-reports/', views.patient_reports, name='patient-reports')
+    path('patient-reports/', views.patient_reports, name='patient-reports'),
+    path('patient-reports/modal/', views.patient_report_modal, name='patient-report-modal')
+    #<int:user_id>/<str:date_updated>/
 ]
--- status/views.py
+++ status/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db.models import Max, Count
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.views.decorators.cache import never_cache
+from django.views.generic import UpdateView
 
 from accounts.models import Patient, Staff
 from symptoms.models import PatientSymptom
@@ -34,7 +35,33 @@ def patient_reports(request):
                 'user__patients_assigned_flags__is_active') \
         .filter(user_id__in=patient_ids).order_by('is_viewed', '-user__patients_assigned_flags__is_active',
                                                   '-date_updated').distinct()
-    
+
     return render(request, 'status/patient-reports.html', {
         'patient_reports': reports
+    })
+
+
+@login_required
+@never_cache
+def patient_report_modal(request):  # , user_id, date_updated):
+
+    reports = PatientSymptom.objects.select_related('user') \
+        .values('date_updated', 'user_id', 'is_viewed', 'user__first_name', 'user__last_name',
+                'user__patients_assigned_flags__is_active') \
+        .filter(user_id__in=[3]).order_by('is_viewed', '-user__patients_assigned_flags__is_active',
+                                          '-date_updated').distinct()
+    user_id = reports[0]['user_id']
+    date_updated = reports[0]['date_updated']
+
+    ################################################
+
+    report_symptom_list = PatientSymptom.objects.select_related('symptom').values('symptom_id', 'data',
+                                                                                  'symptom__name').filter(
+        user_id=user_id,
+        date_updated=date_updated)
+
+    #print(report_symptom_list.query)
+    #print(report_symptom_list)
+    return render(request, 'status/patient-report-modal.html', {
+        'report_symptom_list': report_symptom_list
     })
