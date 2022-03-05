28437c6e2237f2124c71b80c3627add7b8d6d69d
Auvigoo <auvigooa@gmail.com>
2022-03-05 15:18:43 -0500

    Add skeleton for availabilities

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,6 +1,9 @@
 from cProfile import label
+from random import choices
 
 from django import forms
+from django.forms.formsets import BaseFormSet
+
 from appointments.models import Appointment
 
 TIME_SLOTS = [
@@ -49,7 +52,43 @@ ADVANCE = [
     (12, '12 months'),
 ]
 
+DAYS = (('monday', 'Monday'),
+        ('tuesday', 'Tuesday'),
+        ('wednesday', 'Wednesday'),
+        ('thursday', 'Thursday'),
+        ('friday', 'Friday'),
+        ('saturday', 'Saturday'),
+        ('sunday', 'Sunday'))
+
+SLOT_HOURS = [tuple([x, x]) for x in range(0, 24)]
+SLOT_MINUTES = [tuple([x, x]) for x in range(0, 65, 5)]
+
+TIME_HOUR = [tuple([x, x]) for x in range(1, 13)]
+TIME_MINUTE = [tuple([x, str(x).zfill(2)]) for x in range(0, 60, 5)]
+TIME_AM_PM = [('AM', 'AM'), ('PM', 'PM')]
+
+class TimeForm(forms.Form):
+    start_time_hour = forms.IntegerField(widget=forms.Select(choices=TIME_HOUR))
+    start_time_minute = forms.IntegerField(widget=forms.Select(choices=TIME_MINUTE))
+    start_time_am_pm = forms.CharField(widget=forms.Select(choices=TIME_AM_PM))
+
+    end_time_hour = forms.IntegerField(widget=forms.Select(choices=TIME_HOUR))
+    end_time_minute = forms.IntegerField(widget=forms.Select(choices=TIME_MINUTE))
+    end_time_am_pm = forms.CharField(widget=forms.Select(choices=TIME_AM_PM))
+
+class BaseTimeFormSet(BaseFormSet):
+    def clean(self):
+        print(1)
+
+
 
 class AvailabilityForm(forms.Form):
-    availability_slot_time = forms.CharField(label='Time slot', widget=forms.Select(choices=TIME_SLOTS))
-    availability_advance = forms.CharField(label='Advance', widget=forms.Select(choices=ADVANCE))
+    availability_days = forms.MultipleChoiceField(choices=DAYS,
+                                                  widget=forms.CheckboxSelectMultiple())
+
+    slot_duration_hours = forms.IntegerField(widget=forms.Select(choices=SLOT_HOURS))
+    slot_duration_minutes = forms.IntegerField(widget=forms.Select(choices=SLOT_MINUTES))
+
+
+
+
--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,8 @@
+from django.forms import formset_factory
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
-from appointments.forms import AvailabilityForm
+from appointments.forms import AvailabilityForm, TimeForm, BaseTimeFormSet
 from accounts.models import Staff
 
 
@@ -16,6 +17,26 @@ def index(request):
 def add_availabilities(request):
     if request.user.is_staff:
 
+        TimeFormSet = formset_factory(TimeForm, formset=BaseTimeFormSet)
+
+        if request.method == 'POST':
+            availability_form = AvailabilityForm(request.POST)
+            time_formset = TimeFormSet(request.POST)
+
+            if availability_form.is_valid() and time_formset.is_valid():
+
+                print(availability_form.cleaned_data.get('availability_days'))
+                print(availability_form.cleaned_data.get('slot_duration_hours'))
+                print(availability_form.cleaned_data.get('slot_duration_minutes'))
+
+                for time_form in time_formset:
+                    print('start time: %d : %d', time_form.cleaned_data.get('start_time_hour'), time_form.cleaned_data.get('start_time_minute'))
+        else:
+            availability_form = AvailabilityForm()
+            time_formset = TimeFormSet()
+
+
         return render(request, 'appointments/add_availabilities.html', {
-            'form': AvailabilityForm()
+            'availability_form': availability_form,
+            'time_formset': time_formset
         })
