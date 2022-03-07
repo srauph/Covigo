95a95819461e8d3d1535cf7286f98b56a6f98f43
Auvigoo <auvigooa@gmail.com>
2022-03-06 21:31:09 -0500

    Add algorithm to create Appointment objects based on availability forms

--- appointments/views.py
+++ appointments/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from appointments.forms import AvailabilityForm, TimeForm, BaseTimeFormSet
-from accounts.models import Staff
+from datetime import datetime, timedelta
+from appointments.models import Appointment
 
 
 @login_required
@@ -25,16 +26,42 @@ def add_availabilities(request):
 
             if availability_form.is_valid() and time_formset.is_valid():
 
-                print(availability_form.cleaned_data.get('availability_days'))
-                print(availability_form.cleaned_data.get('slot_duration_hours'))
-                print(availability_form.cleaned_data.get('slot_duration_minutes'))
+                availability_days = availability_form.cleaned_data.get('availability_days')
 
-                for time_form in time_formset:
-                    print('start time: ', time_form.cleaned_data.get('start_time_hour'), ':', time_form.cleaned_data.get('start_time_minute'))
+                slot_duration_in_minutes = int(availability_form.cleaned_data.get('slot_duration_hours')) * 60 + \
+                                           int(availability_form.cleaned_data.get('slot_duration_minutes'))
+
+                date_current = datetime.now()
+                # Need to convert from date to datetime object
+                date_until = datetime.combine(availability_form.cleaned_data.get('date_until'), datetime.max.time())
+
+                while date_current <= date_until:
+                    # Only add availabilities at the selected days of the week
+                    if date_current.strftime("%A").lower() in availability_days:
+
+                        # Process each availability time range
+                        for time_form in time_formset:
+                            start = datetime(date_current.year, date_current.month, date_current.day,
+                                             time_form.cleaned_data.get('start_time_hour'),
+                                             time_form.cleaned_data.get('start_time_minute'))
+                            end = datetime(date_current.year, date_current.month, date_current.day,
+                                           time_form.cleaned_data.get('end_time_hour'),
+                                           time_form.cleaned_data.get('end_time_minute'))
+
+                            while start < end:
+                                # Create the individual Appointment objects between each time range with the slot
+                                # duration
+                                next_end = start + timedelta(minutes=slot_duration_in_minutes)
+                                apt = Appointment.objects.create(staff=request.user, patient=None, start_date=start,
+                                                                 end_date=next_end)
+                                apt.save()
+                                start = next_end
+                    # Increment to next day
+                    date_current += timedelta(days=1)
+
         else:
             availability_form = AvailabilityForm()
             time_formset = TimeFormSet()
-
 
         return render(request, 'appointments/add_availabilities.html', {
             'availability_form': availability_form,
