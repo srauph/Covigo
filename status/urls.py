f29b9ce17f46b88c99c37751509835f757ea748f
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2022-03-06 22:28:19 -0500

    Implemented view report modal
    Clicking view report will now show its respective page in the modal

--- status/urls.py
+++ status/urls.py
@@ -5,6 +5,6 @@ app_name = 'status'
 urlpatterns = [
     path('', views.index, name='index'),
     path('patient-reports/', views.patient_reports, name='patient-reports'),
-    path('patient-reports/modal/', views.patient_report_modal, name='patient-report-modal')
+    path('patient-reports/patient-report-modal/<int:user_id>/<str:date_updated>/', views.patient_report_modal, name='patient-report-modal'),
     #<int:user_id>/<str:date_updated>/
 ]
--- status/views.py
+++ status/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
 from django.utils import timezone
+from django.utils.datetime_safe import datetime
 from django.views.decorators.cache import never_cache
 from django.views.generic import UpdateView
 
@@ -45,33 +46,30 @@ def patient_reports(request):
 
 @login_required
 @never_cache
-def patient_report_modal(request):  # , user_id, date_updated):
+def patient_report_modal(request, user_id, date_updated):
+    # When the view report button is pressed a POST request is made
+    if request.method == "POST":
 
-    # Temporarily here to test the page until the modal can function properly
-    reports = PatientSymptom.objects.select_related('user') \
-        .values('date_updated', 'user_id', 'is_viewed', 'user__first_name', 'user__last_name',
-                'user__patients_assigned_flags__is_active') \
-        .filter(user_id__in=[3]).order_by('is_viewed', '-user__patients_assigned_flags__is_active',
-                                          '-date_updated').distinct()
-    user_id = reports[0]['user_id']
-    date_updated = reports[0]['date_updated']
+        # Ensure this was an ajax call
+        if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
 
-    ################################################
+            # Gets all symptoms info required for the report
+            report_symptom_list = PatientSymptom.objects.select_related('symptom', 'user') \
+                .values('symptom_id', 'data', 'symptom__name', 'user__patients_assigned_flags__is_active') \
+                .filter(user_id=user_id, date_updated=date_updated)
 
-    report_symptom_list = PatientSymptom.objects.select_related('symptom', 'user') \
-        .values('symptom_id', 'data', 'symptom__name', 'user__patients_assigned_flags__is_active') \
-        .filter(user_id=user_id, date_updated=date_updated)
+            # Check if the patient is flagged
+            try:
+                is_patient_flagged = Flag.objects.filter(patient_id=user_id).get(is_active=1)
+            except Exception:
+                is_patient_flagged = False
 
-    try:
-        is_patient_flagged = Flag.objects.filter(patient_id=user_id).get(is_active=1)
-    except Exception:
-        is_patient_flagged = False
+            # Render as an httpResponse for the modal to use
+            return HttpResponse(render_to_string('status/patient-report-modal.html', context={
+                'user_id': user_id,
+                'date': date_updated,
+                'report_symptom_list': report_symptom_list,
+                'is_flagged': is_patient_flagged,
+            }, request=request))
 
-    print(is_patient_flagged)
-    context = {
-        'user_id': user_id,
-        'date': date_updated,
-        'report_symptom_list': report_symptom_list,
-        'is_flagged': is_patient_flagged,
-    }
-    return HttpResponse(render_to_string('status/patient-report-modal.html', context=context, request=request))
+    return HttpResponse("Invalid request.")
