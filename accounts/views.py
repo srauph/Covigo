1b9bd6b242019a12ee957ebad37d51a9112132a6
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-03-08 02:03:54 -0500

    rearrange accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -18,12 +18,6 @@ from accounts.utils import (
 )
 
 
-class RegisterPasswordResetConfirmView(PasswordResetConfirmView):
-    def dispatch(self, *args, **kwargs):
-        self.success_url = reverse_lazy('accounts:register_user_password_done', kwargs={'uidb64': kwargs['uidb64']})
-        return super(RegisterPasswordResetConfirmView, self).dispatch(*args, **kwargs)
-
-
 def process_register_or_edit_user_form(request, user_form, profile_form, mode=None):
     user_email = user_form.data.get("email")
     user_phone = profile_form.data.get("phone_number")
@@ -50,105 +44,24 @@ def process_register_or_edit_user_form(request, user_form, profile_form, mode=No
         return False
 
 
-@login_required
-@never_cache
-def two_factor_authentication(request):
-    return render(request, 'accounts/authentication/2FA.html')
-
-
-@never_cache
-def forgot_password(request):
-    if request.method == "POST":
-        password_reset_form = PasswordResetForm(request.POST)
-        if password_reset_form.is_valid():
-            data = password_reset_form.cleaned_data['email']
-            try:
-                user = User.objects.get(email=data)
-                subject = "Covigo - Password Reset Requested"
-                template = "accounts/messages/reset_password_email.html"
-                generate_and_send_email(user, subject, template)
-                return redirect("accounts:forgot_password_done")
-            except MultipleObjectsReturned:
-                password_reset_form.add_error(None, "More than one user with the given email address could be found. Please contact the system administrators to fix this issue.")
-            except User.DoesNotExist:
-                password_reset_form.add_error(None, "No user with the given email address could be found.")
-        else:
-            password_reset_form.add_error(None, "Please enter a valid email address or phone number.")
-    else:
-        password_reset_form = PasswordResetForm()
-    return render(
-        request=request,
-        template_name="accounts/authentication/forgot_password.html",
-        context={"form": password_reset_form}
-    )
-
-
-@never_cache
-def register_user(request, uidb64, token):
-    return redirect('accounts:register_user_password', uidb64=uidb64, token=token)
-
-
-def register_user_password_done(request, uidb64):
-    user = get_user_from_uidb64(uidb64)
-    token = default_token_generator.make_token(user)
-    return redirect('accounts:register_user_details', uidb64, token)
-
-@never_cache
-def register_user_details(request, uidb64, token):
-    INTERNAL_SET_DETAILS_SESSION_TOKEN = "_set_details_token"
-    user = get_user_from_uidb64(uidb64)
-    user_id = user.id
-    valid = False
-
-    if user is not None:
-        if token == 'set-details':
-            session_token = request.session.get(INTERNAL_SET_DETAILS_SESSION_TOKEN)
-            if default_token_generator.check_token(user, session_token):
-                # If the token is valid, display the password reset form.
-                valid = True
-        else:
-            if default_token_generator.check_token(user, token):
-                # Store the token in the session and redirect to the
-                # password reset form at a URL without the token. That
-                # avoids the possibility of leaking the token in the
-                # HTTP Referer header.
-                request.session[INTERNAL_SET_DETAILS_SESSION_TOKEN] = token
-                redirect_url = request.path.replace(token, 'set-details')
-                return redirect(redirect_url, uidb64, token)
-
-    # Process/Create forms if the link is valid
-    if valid:
-        # Process forms
-        if request.method == "POST":
-            user_form = RegisterUserForm(request.POST, instance=user, user_id=user_id)
-            profile_form = RegisterProfileForm(request.POST, instance=user.profile, user_id=user_id)
-
-            if process_register_or_edit_user_form(request, user_form, profile_form):
-                request.session[INTERNAL_SET_DETAILS_SESSION_TOKEN] = None
-                return redirect("accounts:register_user_done")
-
-        # Create forms
-        else:
-            user_form = RegisterUserForm(instance=user, user_id=user_id, initial={"username": None})
-            profile_form = RegisterProfileForm(instance=user.profile, user_id=user_id)
-
-        return render(request, "accounts/registration/register_user_details.html", {
-            "user_form": user_form,
-            "profile_form": profile_form,
-            "validlink": True
-        })
-
-    # Don't process/create forms if the link is expired or invalid
-    else:
-        return render(request, "accounts/registration/register_user_details.html", {
-            "validlink": False
-        })
+def convert_permission_name_to_id(request):
+    permission_array = []
+    for perm in request.POST.getlist('perms'):
+        permission_id = Permission.objects.filter(codename=perm).get().id
+        permission_array.append(permission_id)
+    return permission_array
 
 
 @login_required
 @never_cache
 def index(request):
     return redirect('accounts:list_users')
+
+
+@login_required
+@never_cache
+def two_factor_authentication(request):
+    return render(request, 'accounts/authentication/2FA.html')
 
 
 @login_required
@@ -257,33 +170,6 @@ def edit_user(request, user_id):
 
 
 @login_required
-@never_cache
-def add_group(request):
-    if request.method == 'POST':
-
-        group = Group(name=request.POST['name'])
-        group.save()
-        permission_array = convert_permission_name_to_id(request)
-
-        group.permissions.set(permission_array)
-
-        return redirect('accounts:list_group')
-
-    else:
-        return render(request, 'accounts/access_control/group/add_group.html', {
-            'permissions': Permission.objects.all()
-        })
-
-
-@login_required
-@never_cache
-def list_group(request):
-    return render(request, 'accounts/access_control/group/list_group.html', {
-        'groups': Group.objects.all()
-    })
-
-  
-@login_required
 def flag_user(request, user_id):
     user_staff = request.user
     user_patient = User.objects.get(id=user_id)
@@ -300,20 +186,47 @@ def flag_user(request, user_id):
 
     return redirect("accounts:list_users")
 
-  
+
 @login_required
 def unflag_user(request, user_id):
     user_staff = request.user
     user_patient = User.objects.get(id=user_id)
 
     flag = user_staff.staffs_created_flags.filter(patient=user_patient)
-    
+
     if flag:
         flag = flag.get()
         flag.is_active = False
         flag.save()
-    
+
     return redirect("accounts:list_users")
+
+
+@login_required
+@never_cache
+def list_group(request):
+    return render(request, 'accounts/access_control/group/list_group.html', {
+        'groups': Group.objects.all()
+    })
+
+
+@login_required
+@never_cache
+def add_group(request):
+    if request.method == 'POST':
+
+        group = Group(name=request.POST['name'])
+        group.save()
+        permission_array = convert_permission_name_to_id(request)
+
+        group.permissions.set(permission_array)
+
+        return redirect('accounts:list_group')
+
+    else:
+        return render(request, 'accounts/access_control/group/add_group.html', {
+            'permissions': Permission.objects.all()
+        })
 
 
 @login_required
@@ -336,9 +249,100 @@ def edit_group(request, group_id):
         })
 
 
-def convert_permission_name_to_id(request):
-    permission_array = []
-    for perm in request.POST.getlist('perms'):
-        permission_id = Permission.objects.filter(codename=perm).get().id
-        permission_array.append(permission_id)
-    return permission_array
+@never_cache
+def forgot_password(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            try:
+                user = User.objects.get(email=data)
+                subject = "Covigo - Password Reset Requested"
+                template = "accounts/messages/reset_password_email.html"
+                generate_and_send_email(user, subject, template)
+                return redirect("accounts:forgot_password_done")
+            except MultipleObjectsReturned:
+                password_reset_form.add_error(None, "More than one user with the given email address could be found. Please contact the system administrators to fix this issue.")
+            except User.DoesNotExist:
+                password_reset_form.add_error(None, "No user with the given email address could be found.")
+        else:
+            password_reset_form.add_error(None, "Please enter a valid email address or phone number.")
+    else:
+        password_reset_form = PasswordResetForm()
+    return render(
+        request=request,
+        template_name="accounts/authentication/forgot_password.html",
+        context={"form": password_reset_form}
+    )
+
+
+@never_cache
+def register_user(request, uidb64, token):
+    return redirect('accounts:register_user_password', uidb64=uidb64, token=token)
+
+
+# Yes it's technically not proper to put a class here but since this is a class-based view that in the flow occurs
+# between the register_user and register_user_password_done views, I think it makes more sense to put it here.
+class RegisterPasswordResetConfirmView(PasswordResetConfirmView):
+    def dispatch(self, *args, **kwargs):
+        self.success_url = reverse_lazy('accounts:register_user_password_done', kwargs={'uidb64': kwargs['uidb64']})
+        return super(RegisterPasswordResetConfirmView, self).dispatch(*args, **kwargs)
+
+
+@never_cache
+def register_user_password_done(request, uidb64):
+    user = get_user_from_uidb64(uidb64)
+    token = default_token_generator.make_token(user)
+    return redirect('accounts:register_user_details', uidb64, token)
+
+
+@never_cache
+def register_user_details(request, uidb64, token):
+    internal_set_details_session_token = "_set_details_token"
+    user = get_user_from_uidb64(uidb64)
+    user_id = user.id
+    valid = False
+
+    if user is not None:
+        if token == 'set-details':
+            session_token = request.session.get(internal_set_details_session_token)
+            if default_token_generator.check_token(user, session_token):
+                # If the token is valid, display the password reset form.
+                valid = True
+        else:
+            if default_token_generator.check_token(user, token):
+                # Store the token in the session and redirect to the
+                # password reset form at a URL without the token. That
+                # avoids the possibility of leaking the token in the
+                # HTTP Referer header.
+                request.session[internal_set_details_session_token] = token
+                redirect_url = request.path.replace(token, 'set-details')
+                return redirect(redirect_url, uidb64, token)
+
+    # Process/Create forms if the link is valid
+    if valid:
+        # Process forms
+        if request.method == "POST":
+            user_form = RegisterUserForm(request.POST, instance=user, user_id=user_id)
+            profile_form = RegisterProfileForm(request.POST, instance=user.profile, user_id=user_id)
+
+            if process_register_or_edit_user_form(request, user_form, profile_form):
+                request.session[internal_set_details_session_token] = None
+                return redirect("accounts:register_user_done")
+
+        # Create forms
+        else:
+            user_form = RegisterUserForm(instance=user, user_id=user_id, initial={"username": None})
+            profile_form = RegisterProfileForm(instance=user.profile, user_id=user_id)
+
+        return render(request, "accounts/registration/register_user_details.html", {
+            "user_form": user_form,
+            "profile_form": profile_form,
+            "validlink": True
+        })
+
+    # Don't process/create forms if the link is expired or invalid
+    else:
+        return render(request, "accounts/registration/register_user_details.html", {
+            "validlink": False
+        })
