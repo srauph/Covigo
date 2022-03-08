f167a833c493dca4ab7aa76da0f5472b513dd1ca
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2022-03-07 23:24:33 -0500

    Changed patient report table to datatable

--- status/urls.py
+++ status/urls.py
@@ -5,6 +5,7 @@ app_name = 'status'
 urlpatterns = [
     path('', views.index, name='index'),
     path('patient-reports/', views.patient_reports, name='patient-reports'),
-    path('patient-reports/patient-report-modal/<int:user_id>/<str:date_updated>/', views.patient_report_modal, name='patient-report-modal'),
-    #<int:user_id>/<str:date_updated>/
+    path('patient-reports-table/', views.patient_reports_table, name='patient-reports-table'),
+    path('patient-reports/patient-report-modal/<int:user_id>/<str:date_updated>/', views.patient_report_modal,
+         name='patient-report-modal'),
 ]
