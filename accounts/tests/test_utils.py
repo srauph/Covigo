f568981d7e25266250a30f4fe06729d1851c1ae5
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-03-11 11:30:21 -0500

    refactored test stricture for accounts

--- accounts/tests/test_utils.py
+++ accounts/tests/test_utils.py
@@ -7,15 +7,24 @@ from accounts.utils import get_flag
 
 class GetFlagTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create(username="testuser")
+        self.patient_user = User.objects.create(username="patient_user")
+        self.staff_user = User.objects.create(username="staff_user")
 
     def test_user_without_flag(self):
+        """
+        Test that trying to get a flag that doesn't exist returns None
+        @return:
+        """
         # Act & Assert
-        self.assertIsNone(get_flag(self.user))
+        self.assertIsNone(get_flag(self.staff_user, self.patient_user))
 
     def test_user_with_flag(self):
+        """
+        Test that getting a flag that exists returns the flag
+        @return:
+        """
         # Arrange
-        flag = Flag.objects.create(user=self.user)
+        flag = Flag.objects.create(patient=self.patient_user, staff=self.staff_user)
 
         # Act & Assert
-        self.assertEqual(flag, get_flag(self.user))
+        self.assertEqual(flag, get_flag(self.staff_user,self.patient_user))
