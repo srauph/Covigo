6a9fecd9fe0aa3c519f9328d1674f1521277abbf
Amir Sadeghi <asadeqi@icloud.com>
2022-03-10 23:40:36 -0500

    add utility class

--- status/utils.py
+++ status/utils.py
@@ -7,3 +7,11 @@ def return_reports(patient_ids):
                 'user__patients_assigned_flags__is_active') \
         .filter(user_id__in=patient_ids).distinct()
     return reports
+
+
+def return_symptom_list(user_id, date_updated):
+    report_symptom_list = PatientSymptom.objects.select_related('symptom', 'user') \
+        .values('symptom_id', 'data', 'symptom__name', 'is_viewed', 'user__patients_assigned_flags__is_active',
+                'user__first_name', 'user__last_name') \
+        .filter(user_id=user_id, date_updated=date_updated)
+    return report_symptom_list
