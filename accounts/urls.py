18df0252aab345a66d14a946994fce61e1aba6a2
Shuaib Rauph <89359211+srauph@users.noreply.github.com>
2022-03-12 20:32:36 -0500

    renamed add group to create group, added validation in add/edit group, renamed role to group

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('create/', views.create_user, name='create_user'),
     path('edit/<int:user_id>/', views.edit_user, name='edit_user'),
 
-    path('access_control/group/add', views.add_group, name='add_group'),
-    path('access_control/group/list', views.list_group, name='list_group'),
-    path('access_control/group/edit/<int:group_id>', views.edit_group, name='edit_group'),
+    path('access_control/groups/list/', views.list_group, name='list_group'),
+    path('access_control/groups/create/', views.create_group, name='create_group'),
+    path('access_control/groups/edit/<int:group_id>/', views.edit_group, name='edit_group'),
 
     path('flag/<int:user_id>/', views.flaguser, name='flag_user'),
     path('unflag/<int:user_id>/', views.unflaguser, name='unflag_user'),
