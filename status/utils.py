303b0333d60f6a550ddbefa9a19c82135d28b543
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2022-03-15 21:16:00 -0400

    Fixed patient report table/modal dupes
    Allowing multiple staff to flag a patient caused unforeseen duplicates with the queries

--- status/utils.py
+++ status/utils.py
@@ -1,27 +1,42 @@
+from django.db.models import Q
+
 from symptoms.models import PatientSymptom
 
 
-def return_reports(patient_ids):
-    reports = PatientSymptom.objects.select_related('user') \
+def return_reports(patient_ids, staff_id):
+    criteria = Q(user_id__in=patient_ids) & Q(user__patients_assigned_flags__staff_id=staff_id)
+
+    reports = PatientSymptom.objects.select_related('user').filter(criteria) \
         .values('date_updated__date', 'user_id', 'is_viewed', 'user__first_name', 'user__last_name',
-                'user__patients_assigned_flags__is_active') \
-        .filter(user_id__in=patient_ids).distinct()
+                'user__patients_assigned_flags__is_active', 'user__patients_assigned_flags__staff_id') \
+        .distinct()
     return reports
 
 
-def return_symptom_list(user_id, date_updated):
+def return_symptom_list(user_id, date_updated, staff_id):
+    criteria = Q(user_id=user_id) & Q(date_updated__date=date_updated) & Q(
+        user__patients_assigned_flags__staff_id=staff_id)
+
     report_symptom_list = PatientSymptom.objects.select_related('symptom', 'user') \
+        .filter(criteria) \
         .values('symptom_id', 'data', 'symptom__name', 'is_viewed', 'user__patients_assigned_flags__is_active',
-                'user__first_name', 'user__last_name') \
-        .filter(user_id=user_id, date_updated__date=date_updated)
+                'user__patients_assigned_flags__staff_id', 'user__first_name', 'user__last_name')
     return report_symptom_list
 
 
-def return_symptoms(user_id):
-    return_patient_symptoms = PatientSymptom.objects.select_related('symptom', 'user') \
+def return_symptoms(user_id, staff_id):
+    """
+    Returns a list of symptoms the patient has.
+    @param staff_id:
+    @param user_id: the patient id
+    @return: list of symptoms
+    """
+    criteria = Q(user_id=user_id) & Q(user__patients_assigned_flags__staff_id=staff_id)
+
+    return_patient_symptoms = PatientSymptom.objects.select_related('symptom', 'user').filter(criteria) \
         .values('symptom_id', 'data', 'symptom__name', 'is_viewed', 'user__patients_assigned_flags__is_active',
-                'user__first_name', 'user__last_name') \
-        .filter(user_id=user_id).distinct()
+                'user__patients_assigned_flags__staff_id',
+                'user__first_name', 'user__last_name')
     return return_patient_symptoms
 
 
