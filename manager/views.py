75343bd7a4178adf04d2aad761667ab187e6d655
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-03-16 22:24:27 -0400

    finishes sprint 4 ui

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'manager/index.html')
+    user_count = User.objects.count()
+    return render(request, 'manager/index.html', {"user_count": user_count})
+
+
+def contact_tracing(request):
+    user_count = User.objects.count()
+    return render(request, 'manager/contact_tracing.html', {"user_count": user_count})
