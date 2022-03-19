d6946e4165e5ad84bf2f31b93cfea7158410e2e5
Isaac Muriuki <i_muriuk@live.concordia.ca>
2022-03-19 02:38:58 -0400

    booking appointments works

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,5 +5,5 @@ app_name = 'appointments'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_availabilities/', views.add_availabilities, name='add_availabilities'),
-    path('list_availabilities/', views.list_availabilities, name='list_availabilities')
+    path('book_appointments', views.book_appointments, name='book_appointments')
 ]
