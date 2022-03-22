07eee507de954e372060a6fb886a456a118c38ca
srauph <47877347+srauph@users.noreply.github.com>
2022-03-22 17:03:58 -0400

    Update deploy-server.sh

--- deploy-server.sh
+++ deploy-server.sh
@@ -5,6 +5,7 @@ cd ./tailwind
 npm install -D tailwindcss
 npx tailwindcss -i ../static/Covigo/css/styles.css -o ../static/Covigo/css/dist/styles.css --minify
 cd ..
+python3.10 manage.py migrate
 python3.10 manage.py collectstatic --noinput
 sudo systemctl restart gunicorn.service
 systemctl status -l --no-pager nginx.service
