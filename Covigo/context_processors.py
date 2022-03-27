713addbbf1f68026af45327d581f654d80914d7f
Auvigoo <auvigooa@gmail.com>
2022-03-27 17:22:55 -0400

    Oops, remove import

--- Covigo/context_processors.py
+++ Covigo/context_processors.py
@@ -1,10 +1,6 @@
 from django.conf import settings
-from django.db.models import Q
-
-from messaging.models import MessageGroup
 
 
 def production_mode(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
     return {'PRODUCTION_MODE': settings.PRODUCTION_MODE}
-
