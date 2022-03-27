b45db6f5a844769b5dbfa57609a44442061b490a
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-03-27 19:31:19 -0400

    improved management page

--- manager/views.py
+++ manager/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
+from accounts.models import Patient, Staff
+
 
 def index(request):
     user_count = User.objects.count()
-    return render(request, 'manager/index.html', {"user_count": user_count})
+    patient_count = Patient.objects.count()
+    staff_count = Staff.objects.count()
+    return render(request, 'manager/index.html', {
+        "user_count": user_count,
+        "patient_count": patient_count,
+        "staff_count": staff_count,
+    })
 
 
 def contact_tracing(request):
