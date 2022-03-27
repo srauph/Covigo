da14299676c21399f9bd0c5846f5bf4f7cc28ca6
Isaac Muriuki <i_muriuk@live.concordia.ca>
2022-03-26 21:36:10 -0400

    add json and path in enum

--- Covigo/messages.py
+++ Covigo/messages.py
@@ -10,3 +10,4 @@ class Messages(Enum):
     REGISTER_USER = MESSAGE_TEMPLATES["register_user"]
     RESET_PASSWORD = MESSAGE_TEMPLATES["reset_password"]
     CHANGED_PASSWORD = MESSAGE_TEMPLATES["user_changed_password"]
+    STATUS_UPDATE = MESSAGE_TEMPLATES["status_update"]
