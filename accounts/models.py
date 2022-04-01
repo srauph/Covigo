a8c9d753be48bdb3fdb8f73a84c0737317098da1
Isaac Muriuki <i_muriuk@live.concordia.ca>
2022-04-01 16:47:18 -0400

    added test_results json field to patient

--- accounts/models.py
+++ accounts/models.py
@@ -64,6 +64,7 @@ class Patient(models.Model):
     is_negative = models.BooleanField(default=False)
     is_quarantining = models.BooleanField(default=False)
     code = models.CharField(max_length=255)
+    test_results = models.JSONField(blank=True, null=True)
 
     def get_assigned_staff_user(self):
         try:
