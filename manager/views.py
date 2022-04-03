094a3822447d9a66ee60af6dc16ec1d2d2472cc0
AseelAce <aseelking2015@gmail.com>
2022-04-03 16:41:16 -0400

    Help.html created

--- manager/views.py
+++ manager/views.py
@@ -51,3 +51,7 @@ def contact_tracing(request):
 
     user_count = User.objects.count()
     return render(request, 'manager/contact_tracing.html', {"user_count": user_count})
+
+
+def help_page(request):
+    return render(request, 'manager/help.html')
