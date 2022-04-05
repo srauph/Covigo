ba3ab4deba62abfa0faeebcc24c93e1c1a19168c
shahdyousefak <shahdyousefak@gmail.com>
2022-04-05 00:49:51 -0400

    oops

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from messaging import models
-admin.site.register(models.MessageContent)
 from messaging.models import MessageGroup, MessageContent
 
 @admin.register(MessageGroup)
