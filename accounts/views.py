b7ed3c24918e0b00324d6ffc837ef0b644ee4908
Shuaib Rauph <47877347+srauph@users.noreply.github.com>
2022-04-06 22:44:38 -0400

    fixed postal code

--- accounts/views.py
+++ accounts/views.py
@@ -347,10 +347,12 @@ def profile(request, user_id):
     )
 
     perms_view_appointments = (
-        user == request.user
-        or request.user.has_perm("accounts.view_user_appointment")
-        or request.user.has_perm("accounts.view_patient_appointment") and not user.is_staff
-        or request.user.has_perm("accounts.is_doctor") and user in request.user.staff.get_assigned_patient_users()
+        not user.is_superuser and (
+            user == request.user
+            or request.user.has_perm("accounts.view_user_appointment")
+            or request.user.has_perm("accounts.view_patient_appointment") and not user.is_staff
+            or request.user.has_perm("accounts.is_doctor") and user in request.user.staff.get_assigned_patient_users()
+        )
     )
 
 
@@ -492,9 +494,13 @@ def profile(request, user_id):
 
     # If profile belongs to a staff member
     else:
-        doctor_id = Staff.objects.filter(user_id=user.id).first().id
-        appointments = Appointment.objects.filter(staff_id=doctor_id).filter(all_filter).order_by("start_date")
-        appointments_truncated = appointments[:4]
+        if not user.is_superuser:
+            doctor_id = Staff.objects.filter(user_id=user.id).first().id
+            appointments = Appointment.objects.filter(staff_id=doctor_id).filter(all_filter).order_by("start_date")
+            appointments_truncated = appointments[:4]
+        else:
+            appointments = []
+            appointments_truncated = []
         assigned_patients = [] if user.is_superuser else user.staff.get_assigned_patient_users()
         issued_flags = Flag.objects.filter(staff=user, is_active=True)
 
